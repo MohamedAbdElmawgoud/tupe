a347dcac3a5535f1ec4d488e12d3fbb8daa36ce3
nader medhet <nadermedhet142@gmail.com>
2020-04-16 21:24:06 +0200

    add video links

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Router } from "@angular/router";
 import { FirebaseService } from "src/app/firebase/firebase.service";
-import { ModalController } from '@ionic/angular';
-import { CreateCompPage } from "src/app/create-comp/create-comp.page";
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab1',
@@ -18,7 +18,7 @@ export class Tab1Page {
     constructor(
     private platform: Platform,
     private firebase:FirebaseService,
-    private modalController:ModalController,
+    public alertController: AlertController ,
     public router: Router) {
   
   }
@@ -28,71 +28,48 @@ export class Tab1Page {
    }
   
   
-  createCompinge() {
+  async createCompinge() {
+    const alert = await this.alertController.create({
+      header: 'select the camping type',
+      inputs: [
+
+        {
+          name: 'view for you video',
+          type: 'radio',
+          label: 'view for your video',
+          value: 'video'
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: ' likes or subscribes  ',
+          value: 'channel'
+        }
+      ],
+      buttons : [
+        {
+          text : "select"
+        }
+      ]
+    });
+
+    await alert.present();
+
+    alert.onDidDismiss().then(e=>{
+      if(e.data){
+        this.router.navigate(['create-comp'] , {
+          queryParams : {
+            type : e.data.values
+          }
+        });
+
+      }
+      
+    })
 
   // this.presentModal();
-   this.router.navigate(['create-comp']);
    
     
   }
-  // async presentModal() {
-  //   const modal = await this.modalController.create({
-  //     component: CreateCompPage,
-     
-  //   });
-  //   await modal.present()
-  //   modal.onDidDismiss().then(e => {
-  //     this.type
-  //   })
-      
-     
-  //   return await modal.present();
-    
-  // }
-  // loginUser(): void {//myreversedclientid
-  //   this.googlePlus.login({
-  //     'webClientId': '167757336992-0aqqo9rn80ledvcb2pmn3fqblb36oef6.apps.googleusercontent.com',
-  //     'offline': true
-  //   }).then( res => {
-  //     firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
-  //       .then( success => {
-  //         console.log("Firebase success: " + JSON.stringify(success));
-  //       })
-  //       .catch( error => console.log("Firebase failure: " + JSON.stringify(error)));
-  //    }).catch(err => console.error("Error: ", err));
-  // }
-  // login() {
-  //   if (this.platform.is('cordova')) {
-  //     this.nativeGoogleLogin();
-  //   }
-  //   else {
-  //     this.webGoogleLogin();
-  //   }
-
-  // } 
-
-  // async nativeGoogleLogin() {
-  //   try {
-  //     const user = await this.googlePlus.login({
-
-  //       'webClientId': '941729484801-qjc4h0cv1b4d4c9s7ms7q8a48gie573e.apps.googleusercontent.com',
-  //       'offline': true
-  //     })
-  //     return await this.angularFireAuth.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(user.idTocken))
-  //   }
-  //   catch (err) {
-  //     console.log(err)  
-  //   }
-
-  // }
-
-  // webGoogleLogin() {
-  //   try {
-  //     const provider = new firebase.auth.GoogleAuthProvider();
-  //     const credential = new this.angularFireAuth.auth.signInWithPopup(provider)
-  //   }
-  //   catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+  
 }
