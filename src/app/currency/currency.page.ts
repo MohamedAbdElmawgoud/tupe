6edabe10079795dd6b3e18ac612fca2e7e03ab85
nader medhet <nadermedhet142@gmail.com>
2020-04-17 23:55:31 +0200

    add reward ad

--- src/app/currency/currency.page.ts
+++ src/app/currency/currency.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free/ngx';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Component({
   selector: 'app-currency',
@@ -10,7 +11,8 @@ import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free/ngx';
 export class CurrencyPage implements OnInit {
 
   constructor(public router: Router,
-    private admobFree: AdMobFree
+    private admobFree: AdMobFree,
+    private firebaseService: FirebaseService
   ) { }
 
   ngOnInit() {
@@ -31,17 +33,32 @@ export class CurrencyPage implements OnInit {
       // for the sake of this example we will just use the test config
       isTesting: true,
       autoShow: true,
-      id : "ca-app-pub-6473277362437826/2644499851"
-     };
-     this.admobFree.banner.config(bannerConfig);
-     
-     this.admobFree.interstitial.prepare()
-       .then(() => {
-         // banner Ad is ready
-         // if we set autoShow to false, then we will need to call the show method here
-       })
-       .catch(e => console.log(e));
+      id: "ca-app-pub-6473277362437826/2644499851"
+    };
+    this.admobFree.rewardVideo.config(bannerConfig);
 
+    this.admobFree.rewardVideo.prepare()
+      .then(() => {
+
+      })
+      .catch(e => console.log(e));
+    this.admobFree.rewardVideo.show().then(() => {
+
+    })
+
+    document.addEventListener('admob.rewardvideo.events.REWARD', (result) => {
+      let user = this.firebaseService.getDataOfUser().subscribe(e => {
+        console.log('user after update :', e);
+        let UserEdited = {
+          displayName: e.displayName,
+          photoURL: e.photoURL,
+          email: e.email,
+          uid: e.uid,
+          point: e.point + 20
+        }
+        this.firebaseService.updateUserData(UserEdited)
+      });
+    });
   }
 
 }
