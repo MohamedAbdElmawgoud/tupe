3d8eea4e6af598e1b167ce7fbf358278f9cddd39
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-04-19 17:50:43 +0200

    ui

--- src/app/create-comp/create-comp.page.ts
+++ src/app/create-comp/create-comp.page.ts
@@ -20,7 +20,13 @@ export class CreateCompPage implements OnInit {
   showPoint: any;
 
   id: string;
-  // numberOfSubscribers = [10, 20, 30, 40, 50, 60, 70, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+  Views = [10, 50,  100,150, 200,250, 300,350, 400,450
+    , 500,550, 600,650, 700,750, 800,850, 900,950, 1000]
+    seconds= [45,
+      60,90,120,150,180,210,240,270,300,330,360,390,420,450,
+      480,510,540,570,600,900,1200,1500,1800,
+      2100,2400,2700,3000,3300,3600
+    ]
   likes = 10;
   Subscribe = 10;
   view = 10;
@@ -28,7 +34,7 @@ export class CreateCompPage implements OnInit {
   needed: number;
   ListOfUserDoneIt: string[];
   camping: camping;
-  sec = 10;
+  sec = 45;
   point = this.view * this.sec;
   video;
   videoId;
