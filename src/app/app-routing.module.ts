431a7fb39f09307ab107fac62f72ae358216c655
nader medhet <nadermedhet142@gmail.com>
2020-04-24 14:53:33 +0200

    fix auth login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,58 +1,82 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    canActivate : [AuthGuard]
   },
   {
     path: 'tabs/currency',
-    loadChildren: () => import('./currency/currency.module').then( m => m.CurrencyPageModule)
+    loadChildren: () => import('./currency/currency.module').then( m => m.CurrencyPageModule),
+    canActivate : [AuthGuard]
   },
   {
     path: 'invite-friends',
-    loadChildren: () => import('./invite-friends/invite-friends.module').then( m => m.InviteFriendsPageModule)
+    loadChildren: () => import('./invite-friends/invite-friends.module').then( m => m.InviteFriendsPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'buy-point',
-    loadChildren: () => import('./buy-point/buy-point.module').then( m => m.BuyPointPageModule)
+    loadChildren: () => import('./buy-point/buy-point.module').then( m => m.BuyPointPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'subscription',
-    loadChildren: () => import('./subscription/subscription.module').then( m => m.SubscriptionPageModule)
+    loadChildren: () => import('./subscription/subscription.module').then( m => m.SubscriptionPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'create-comp',
-    loadChildren: () => import('./create-comp/create-comp.module').then( m => m.CreateCompPageModule)
+    loadChildren: () => import('./create-comp/create-comp.module').then( m => m.CreateCompPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'faq',
-    loadChildren: () => import('./faq/faq.module').then( m => m.FaqPageModule)
+    loadChildren: () => import('./faq/faq.module').then( m => m.FaqPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'message',
-    loadChildren: () => import('./message/message.module').then( m => m.MessagePageModule)
+    loadChildren: () => import('./message/message.module').then( m => m.MessagePageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'vip-account',
-    loadChildren: () => import('./vip-account/vip-account.module').then( m => m.VipAccountPageModule)
+    loadChildren: () => import('./vip-account/vip-account.module').then( m => m.VipAccountPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'add-link',
-    loadChildren: () => import('./add-link/add-link.module').then( m => m.AddLinkPageModule)
+    loadChildren: () => import('./add-link/add-link.module').then( m => m.AddLinkPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'log-in',
-    loadChildren: () => import('./log-in/log-in.module').then( m => m.LogInPageModule)
+    loadChildren: () => import('./log-in/log-in.module').then( m => m.LogInPageModule),
+    
   },
   {
     path: 'details-campaign',data: {data : ''},
-    loadChildren: () => import('./details-campaign/details-campaign.module').then( m => m.DetailsCampaignPageModule)
+    loadChildren: () => import('./details-campaign/details-campaign.module').then( m => m.DetailsCampaignPageModule),
+    canActivate : [AuthGuard]
+
   },
   {
     path: 'create-subscripe',
-    loadChildren: () => import('./create-subscripe/create-subscripe.module').then( m => m.CreateSubscripePageModule)
+    loadChildren: () => import('./create-subscripe/create-subscripe.module').then( m => m.CreateSubscripePageModule),
+    canActivate : [AuthGuard]
+
   }
 ];
 @NgModule({
