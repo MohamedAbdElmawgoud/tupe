878616647504143a40fd56dcea458b36a7398f8b
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-05-07 19:44:33 +0200

    fix

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,245 @@
+import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Router } from "@angular/router";
+import { FirebaseService } from "src/app/firebase/firebase.service";
+import { AlertController } from '@ionic/angular';
+import { TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { CampingsService, camping } from "src/app/firebase/campings.service";
+import { map } from "rxjs/operators";
+import { StorageService } from '../storageService/storage.service';
+import { AdMobPro } from '@ionic-native/admob-pro/ngx';
+import { subscribesService } from '../firebase/subscripe';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  compaignView =[]
+  showPoint: any;
+  user: any;
+  view = [];
+  viewer = [];
+  done = 0;
+  createdDate = [];
+  uid: string;
+  compaignValue = [];
+  points: number;
+  type: any;
+  userProfile: any = null;
+  campings
+  compaign = false;
+  constructor(
+    private platform: Platform,
+    private firebase: FirebaseService,
+    private translate: TranslateService,
+    private campingsService: CampingsService,
+    public alertController: AlertController,
+    private storage: StorageService,
+    private admob: AdMobPro,
+    private subscribes: subscribesService,
+    public router: Router) {
+
+  }
+  async ngOnInit() {
+
+
+   
+    this.getPoint()
+
+    this.getUserId()
+    this.user = await this.storage.getUserId();
+    let status= false;
+    this.firebase.getDataOfUser(this.user).then(user =>{
+       status = user.docs[0].data().vip.status
+       console.log('vip is ' ,status)
+      if(status == false){
+       
+        this.admob.createBanner({
+          adId:
+            "ca-app-pub-3736449894948823/3846950634"
+        })
+          .then(() => { this.admob.showBanner(this.admob.AD_POSITION.BOTTOM_CENTER); });
+       
+      }
+    })
+   
+  }
+
+  getPoint() {
+    this.firebase.getDataOfUser(this.user).then(point => {
+      this.showPoint = point.docs[0].data().point
+    })
+    return this.showPoint
+  }
+  googleSignin() {
+    this.firebase.googleSignin();
+  }
+  ionViewWillEnter() {
+    this.getCompinge()
+    this.getPoint()
+  }
+  getCompinge() {
+   
+    let done = 0;
+    this.storage.getUserId().then(user => {
+      if (!user) {
+        console.log('go to logIn')
+        this.router.navigate(['log-in']);
+      }
+      else {
+       
+
+        this.subscribes.getsubscribesList((res =>
+          res.orderByChild('ownerId')
+            .equalTo(user))).snapshotChanges().pipe(
+              map((changes: Array<any>) =>
+                changes.map(c =>
+                  ({ key: c.payload.key, ...c.payload.val() })
+                )
+              )
+            ).subscribe(subscribes => {
+              this.compaignValue =[]              
+              subscribes.forEach(ele => {
+
+                let views = `${ele.view}/${ele.done ? ele.done.length : 0}`
+                ele['viewStat'] = views;
+                ele['type'] = 'subscribe'
+                ele['image'] = ele.channel.channel.thumbnails.default.url;
+
+                this.compaignValue.push(ele)
+
+              })
+              console.log(this.compaignValue);
+
+            });
+
+            this.campingsService.getcampingsList((res =>
+              res.orderByChild('ownerId')
+                .equalTo(user))).snapshotChanges().pipe(
+                  map((changes: Array<any>) =>
+                    changes.map(c =>
+                      ({ key: c.payload.key, ...c.payload.val() })
+                    )
+                  )
+                ).subscribe(comp => {
+                  this.compaignView =[]
+                  comp.forEach(ele => {
+                    let views = `${ele.view}/${ele.done ? ele.done.length : 0}`
+                    ele['viewStat'] = views;
+                    this.compaignView.push(ele)
+
+                  })
+                  console.log('compaignView is', this.compaignView)
+                });
+
+      }
+      //return user;
+    })
+
+
+
+
+
+  }
+  getDetailsOfComp(createdate) {
+    this.router.navigate(['details-campaign'], { queryParams: { data: createdate.key , type : createdate.type  } });
+  }
+  getUserId() {
+
+  }
+  async createCompinge() {
+    let status;
+   await this.firebase.getDataOfUser(this.user).then(status=> {
+      status = (status.docs[0].data().vip || {}).status
+     
+   
+ 
+  if(status){
+    console.log('status for vip',status)
+    if (this.compaignValue.length >= 20) {
+     
+      let  header= this.translate.instant('you have reached your limit')
+     
+this.presentAlert(header)
+     
+      return;
+    }
+    else {
+    if (this.compaignValue.length >= 5) {
+     
+      let  header= this.translate.instant('you have reached your limit')
+      this.presentAlert(header)
+      return;
+    }
+  }
+  }
+})
+  
+    const alert = await this.alertController.create({
+      header: this.translate.instant('select the camping type'),
+      inputs: [
+
+        {
+          name: 'view for you video',
+          type: 'radio',
+          label: this.translate.instant('view for your video'),
+          value: 'video'
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: this.translate.instant('likes or subscribes'),
+          value: 'channel'
+        }
+      ],
+      buttons: [
+        {
+          text: this.translate.instant("select")
+        }
+      ]
+    });
+
+    await alert.present();
+
+    alert.onDidDismiss().then(e => {
+      if (e.data) {
+
+        if (e.data.values == 'channel') {
+          this.router.navigate(['create-subscripe'], {
+            queryParams: {
+              type: e.data.values
+            }
+          });
+        } else {
+          this.router.navigate(['create-comp'], {
+            queryParams: {
+              type: e.data.values
+            }
+          });
+        }
+
+
+      }
+
+    })
+
+    // this.presentModal();
+
+
+  }
+  async presentAlert(title) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      // subHeader: 'Subtitle',
+      message: title,
+      buttons: ['OK'],
+    });
+    setTimeout(() => {
+      this.alertController.dismiss()
+    }, 3000)
+    await alert.present();
+  }
+}
